6838cbdf208bea151d8b50a261e4d48af7581f44
Gusta169 <gustavoliveirablu1298@gmail.com>
2025-09-23 09:03:22 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+package primeiroprograma; //criação do pacote
+
+public class Main { //letra maiscula, para determinar a classe
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo");
+    }
+}

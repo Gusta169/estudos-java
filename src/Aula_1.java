d420951e794fb6b7c6d729719079f384d6bc2f84
Gusta169 <gustavoliveirablu1298@gmail.com>
2026-06-26 21:01:04 -0300

    Refect | Recreated my principal folder

--- src/Aula_1.java
+++
@@ -1,7 +0,0 @@
-package primeiroprograma; //criação do pacote
-
-public class Main { //letra maiscula, para determinar a classe
-    public static void main(String[] args) {
-        System.out.println("Ola Mundo");
-    }
-}
